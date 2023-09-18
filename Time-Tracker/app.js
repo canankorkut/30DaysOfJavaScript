f183829f0200485ba7a163911634d411ec587386
Canan Korkut <99427828+canankorkut@users.noreply.github.com>
2023-09-19 01:58:13 +0300

    Updated app.js

--- Time-Tracker/app.js
+++ Time-Tracker/app.js
@@ -2,13 +2,31 @@ const addButton = document.querySelector("#addTask")
 const taskContainer = document.querySelector(".task-container")
 const taskInput = document.querySelector("input[type='text']")
 
+let timerInterval // Stores the timer
+let isTimerRunning = false;
+let seconds = 0 // Variable used to start the timer
+
+// Updates the timer
+function updateTimer() {
+  seconds++;
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  const timeDisplay = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
+  const time = document.querySelector(".time");
+  time.textContent = timeDisplay;
+}
+
+// When the Add task button is clicked
 addButton.addEventListener("click", () => {
     const taskTitleValue = taskInput.value.trim()
 
     if(taskTitleValue.trim() === "") {
         return
     }
-    
+
+    // New task is created
     const newTaskDiv = document.createElement("div")
     newTaskDiv.classList.add("task", "d-flex", "justify-content-between", "flex-column", "mt-3")
     
@@ -17,8 +35,11 @@ addButton.addEventListener("click", () => {
     taskTitle.innerHTML = `<p>${taskTitleValue}</p>`
 
     const timeDiv = document.createElement("div")
-    timeDiv.classList.add("time")
-    timeDiv.textContent = "00:00:00"
+    timeDiv.classList.add("timeDiv")
+    
+    const time = document.createElement("div")
+    time.classList.add("time")
+    time.textContent = "00:00:00"
 
     const timeActions = document.createElement("div")
     timeActions.classList.add("time-actions")
@@ -36,25 +57,63 @@ addButton.addEventListener("click", () => {
     resetButton.textContent = "Reset"
 
     const taskDelete = document.createElement("div")
-    taskDelete.classList.add("task-edit", "mt-2")
+    taskDelete.classList.add("taskDelete", "mt-2")
 
     const deleteButton = document.createElement("button")
     deleteButton.classList.add("delete", "btn", "btn-outline-danger")
     deleteButton.textContent = "Delete Task"
 
+    // Clicking the Delete button deletes the task
+    deleteButton.addEventListener("click", () => {
+        // Finds the parent (newTaskDiv) of the task to manage
+        const taskItem = deleteButton.closest(".task")
+
+        // Removes this task from taskContainer
+        taskContainer.removeChild(taskItem)
+    });
+
+    // Start the timer when you click the start button
+    startButton.addEventListener("click", () => {
+        if (!timerInterval) {
+            timerInterval = setInterval(updateTimer, 1000);
+            isTimerRunning = true;
+        }
+    });
+
+    // Clicking the stop button stops the timer
+    stopButton.addEventListener("click", () => {
+        clearInterval(timerInterval);
+        isTimerRunning = false;
+    });
+
+    // Clicking the reset button resets the timer
+    resetButton.addEventListener("click", () => {
+        const time = document.querySelector(".time");
+        if (isTimerRunning) {
+            clearInterval(timerInterval);
+            isTimerRunning = false;
+        }
+        seconds = 0;
+        time.textContent = "00:00:00"
+    });
+
+    // Adds all created elements to new task div
+    timeDiv.appendChild(time)
     timeDiv.appendChild(timeActions)
-    timeActions.appendChild(startButton)
-    timeActions.appendChild(stopButton)
-    timeActions.appendChild(resetButton)
+    timeActions.appendChild(startButton);
+    timeActions.appendChild(stopButton);
+    timeActions.appendChild(resetButton);
 
-    taskDelete.appendChild(deleteButton)
+    taskDelete.appendChild(deleteButton);
 
-    newTaskDiv.appendChild(taskTitle)
-    newTaskDiv.appendChild(timeDiv)
-    newTaskDiv.appendChild(taskDelete)
+    newTaskDiv.appendChild(taskTitle);
+    newTaskDiv.appendChild(timeDiv);
+    newTaskDiv.appendChild(taskDelete);
 
-    taskContainer.appendChild(newTaskDiv)
+    // Adds the new task div to the task container
+    taskContainer.appendChild(newTaskDiv);
 
+    // Clears input field after task is added
     taskInput.value = ""
-
 })
+
