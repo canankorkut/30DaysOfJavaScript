c3bb0694dff57a049186107a8cc349abd890b21f
Canan Korkut <99427828+canankorkut@users.noreply.github.com>
2023-09-21 01:28:04 +0300

    Updated app.js

--- Recipe-App/app.js
+++ Recipe-App/app.js
@@ -3,20 +3,26 @@ const API_ID = '...'
 const recipeDiv = document.querySelector(".recipe")
 const searchButton = document.querySelector("#searchButton")
 
+// Defines the function to perform a search query
 async function searchRecipe() {
     try {
+        // Food name entered by the user
         const searchInput = document.getElementById("searchInput").value
+
+        // Sends a request to retrieve recipes from the API
         const response = await fetch(`https://api.edamam.com/search?q=${searchInput}&app_id=${API_ID}&app_key=${API_KEY}`)
  
         if (!response.ok) {
-          throw new Error('API Hatası: İstek başarısız.')
+          throw new Error('API Error: Request failed.')
         }
     
         const data = await response.json()
-    
+
+        // Processes recipes using data from the API
         const randomIndex = Math.floor(Math.random() * data.hits.length)
         const randomRecipe = data.hits[randomIndex].recipe
 
+        // Adds the recipe results to the recipe div
         recipeDiv.innerHTML = `
             <h3>${randomRecipe.label}</h3>
             <img src="${randomRecipe.image}" alt="image of food">
@@ -26,7 +32,8 @@ async function searchRecipe() {
             </ul>
         `
     } catch (error) {
-        console.error('Api error:', error)
+        console.error('API Error:', error)
     }  
 }
+// Starts the search process when the search button is clicked
 searchButton.addEventListener('click', searchRecipe)
